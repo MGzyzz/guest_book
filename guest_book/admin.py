4d854ad25c47b69a3939be81f0b582fb7bbb6e6a
Madi <madi_gaziz@mail.ru>
2023-07-02 12:07:57 +0600

    Сделал отображение и добавление данных в админ панель

--- guest_book/admin.py
+++ guest_book/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Guest_book
 # Register your models here.
+
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'email_author', 'time_of_creation', 'edit_time', 'status']
+    list_filter = ['name', 'status']
+    search_fields = ['name']
+
+
+admin.site.register(Guest_book, GuestBookAdmin)
